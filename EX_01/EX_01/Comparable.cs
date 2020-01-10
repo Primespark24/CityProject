6e7232d48a63cebb5b662ab5092ec503e62e5793
Brycen Martin <bmartin22@my.whitworth.edu>
2020-01-09 19:42:56 -0800

    Added Icomparable Interface for use

---
+++ EX_01/EX_01/Comparable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_01
+{
+    class Comparable
+    {
+        public interface IComparable
+        {
+            void CompareArea();
+            void ComparePerimeter();
+            void CompareColor();
+        }
+    }
+}
